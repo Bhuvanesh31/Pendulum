8995b3f3142cec145d5e1a155b14cb851e9dcb45
xxpittip <pit.duw@gmail.com>
2018-02-27 23:54:43 +0100

    added progressive plot

--- pendulum.py
+++ pendulum.py
@@ -4,17 +4,26 @@ import matplotlib.pyplot as plt
 
 theta_c = 1
 theta_dot_c = 0.1
-dt = 0.001
-steps = 10000
+dt = 0.01
+steps = 1000
 g = 9.81
 l_1 = 1
-
+time = np.arange(0,dt*steps,dt)
+alphas = np.arange(0,1,1/steps)
+rgba_colors = np.zeros((100,4))
+rgba_colors[:,0] = 0.2
+rgba_colors[:,1] = 0.7
+rgba_colors[:,2] = 0.5
+rgba_colors[:,3] = np.linspace(0.,1.,100)
 theta = np.zeros(steps)
 theta_dot = np.zeros(steps)
 
+plt.axis([0, steps*dt, -np.pi, np.pi])
+plt.ion()
+plt.show()
+
 def DGL_single_theta(params):
     theta_dot = params[1]
-
     return  theta_dot
 
 def DGL_single_theta_dot(params):
@@ -25,14 +34,21 @@ def exp_Eul(DGLs, vals):
     result = []
     for i, DGL in enumerate(DGLs):
         result.append(vals[i] + dt*DGL(vals))
-    print(result)
     return result
 
 for i in range(steps):
     theta[i], theta_dot[i] = exp_Eul((DGL_single_theta, DGL_single_theta_dot), (theta_c, theta_dot_c))
-
     theta_c = theta[i]
     theta_dot_c = theta_dot[i]
 
-plt.plot(theta)
-plt.show()
+    if not i%10:
+        a = (i>100)*(i-100)
+        b = len(time[a:i])-1
+        print(len(time[a:i]))
+        print(a, i)
+        if i > 0:
+            pts.remove()
+        pts = plt.scatter(time[a:i], theta[a:i], color=rgba_colors[-b:,:])
+
+
+        plt.pause(0.0001)
